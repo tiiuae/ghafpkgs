ccced38fbf5d801ee54bc532e101bc21f8151d02
Ganga Ram <Ganga.Ram@tii.ae>
2025-09-16 17:21:08 +0400

    vsock-bridge python package
    Provides following functionalities:
    - secure communication between host and guest
    - defines protocol for device passthrough management
    
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

---
+++ packages/vsock-bridge/vsock_bridge/src/vsock_bridge/json_transport.py
@@ -0,0 +1,30 @@
+# Copyright 2022-2025 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+import socket
+from collections.abc import Generator
+from typing import Any
+from vsock_bridge.logger import logger
+
+
+def send(sock: socket.socket, obj: dict[str, Any]) -> None:
+    data = (json.dumps(obj, separators=(",", ":")) + "\n").encode("utf-8")
+    sock.sendall(data)
+
+
+def receive(sock: socket.socket) -> Generator[dict[str, Any], None, None]:
+    buf = b""
+    while True:
+        chunk = sock.recv(4096)
+        logger.debug(f"JSON received a chunk {chunk}...parsing...")
+        if not chunk:
+            break
+        buf += chunk
+        while b"\n" in buf:
+            line, buf = buf.split(b"\n", 1)
+            line = line.strip()
+            logger.debug(f"JSON  parsing line {line}")
+            if line:
+                yield json.loads(line.decode("utf-8"))
+        logger.debug("JSON parsing done.")
