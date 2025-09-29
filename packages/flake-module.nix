a313a9d1d8292df267a400535f5a60fd4b9b977e
Manuel Bluhm <manuel@ssrc.tii.ae>
2025-09-29 10:56:52 +0400

    feat(ldap): shell utility script
    Small shell utility script to query LDAP, currently used to show user
    information in Active Directory setup script.
    
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -15,6 +15,7 @@ let
       ghaf-nw-packet-forwarder = callPackage ./ghaf-nw-packet-forwarder { inherit (inputs) crane; };
       ghaf-wallpapers = callPackage ./ghaf-wallpapers { };
       hotplug = python3Packages.callPackage ./hotplug/package.nix { };
+      ldap-query = python3Packages.callPackage ./ldap-query/package.nix { };
       vhotplug = python3Packages.callPackage ./vhotplug/package.nix { };
       vinotify = python3Packages.callPackage ./vinotify/package.nix { };
     };
