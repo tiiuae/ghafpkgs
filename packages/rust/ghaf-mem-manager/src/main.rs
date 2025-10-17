6745477f1bc0e6b7b425ace155920609e48956a9
Santtu Lakkala <santtu.lakkala@unikie.com>
2025-10-17 15:53:33 +0300

    mem-manager: Manage VMs in single process
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- packages/rust/ghaf-mem-manager/src/main.rs
+++ packages/rust/ghaf-mem-manager/src/main.rs
@@ -4,184 +4,178 @@
  */
 use anyhow::Result;
 use clap::Parser;
-use std::{
-    collections::HashMap,
-    path::PathBuf,
-    time::{Duration, Instant},
-};
-use tracing::{debug, info, warn};
+use std::{collections::HashMap, path::PathBuf, sync::Arc};
+use tokio::sync::RwLock;
+use tracing::{debug, warn};
 
 mod qmp;
-use qmp::QmpEndpoint;
+mod vm;
+
+#[derive(Default)]
+struct VMData {
+    errors: usize,
+}
+
+#[derive(Clone)]
+struct MemManager {
+    low: u8,
+    high: u8,
+    machines: Arc<RwLock<HashMap<vm::VM, VMData>>>,
+}
+
+fn host_available_memory() -> Result<usize> {
+    use std::io::BufRead;
+    let mi = std::io::BufReader::new(
+        std::fs::OpenOptions::new()
+            .read(true)
+            .open("/proc/meminfo")?,
+    );
+    for l in mi.lines() {
+        if let Some(avail) = l?.strip_prefix("MemAvailable:")
+            && let Some(avail) = avail.split_ascii_whitespace().next()
+        {
+            return avail.parse().map(|kb: usize| kb * 1024).map_err(Into::into);
+        }
+    }
+    Err(anyhow::anyhow!("Unable to read available memory"))
+}
+
+impl MemManager {
+    fn new(low: u8, high: u8) -> Self {
+        Self {
+            machines: Arc::new(RwLock::new(HashMap::new())),
+            low,
+            high,
+        }
+    }
+
+    #[allow(
+        clippy::cast_sign_loss,
+        clippy::cast_possible_truncation,
+        clippy::cast_precision_loss
+    )]
+    async fn manage(&self) -> Result<()> {
+        let mut mem = host_available_memory()?;
+        let mut reqd = 0;
+        let mut min = 0;
+        let mut machines = self.machines.write().await;
+        let mut infos = Vec::new();
+        let mut err = false;
+
+        for (m, data) in machines.iter_mut() {
+            min += m.minimum;
+            if let Ok(info) = m.preferred_memory_size(self.low, self.high).await {
+                debug!(
+                    "VM {m} with {current} requested {preferred}",
+                    current = info.current,
+                    preferred = info.preferred,
+                );
+                mem += info.current;
+                reqd += info.preferred;
+                infos.push(info);
+                data.errors = 0;
+            } else {
+                data.errors += 1;
+                err |= true;
+            }
+        }
+
+        if err {
+            for (m, _) in machines.extract_if(|_, d| d.errors >= 5) {
+                warn!("Connecting to {m} failed 5 times, dropping");
+            }
+            anyhow::bail!("Failed to connect to some VMs, retrying later");
+        }
+        let machines = machines.downgrade();
+
+        let scale = if reqd <= mem {
+            1.0
+        } else if reqd == min {
+            0.0
+        } else {
+            warn!("Memory limited, VMs requested {reqd}, available {mem}");
+            (mem - min) as f32 / (reqd - min) as f32
+        };
+
+        for (machine, info) in machines.keys().zip(infos) {
+            machine
+                .adjust(
+                    ((info.preferred - machine.minimum) as f32 * scale) as usize + machine.minimum,
+                )
+                .await
+                .ok();
+        }
+
+        Ok(())
+    }
+}
+
+#[zbus::interface(name = "ae.tii.MemManager", spawn = false)]
+impl MemManager {
+    async fn attach_vm(
+        &self,
+        socket: PathBuf,
+        minimum: u64,
+        maximum: u64,
+    ) -> Result<(), zbus::fdo::Error> {
+        let Ok(minimum) = usize::try_from(minimum) else {
+            return Err(zbus::fdo::Error::InvalidArgs("Minimum out-of-range".into()));
+        };
+        let Ok(maximum) = usize::try_from(maximum) else {
+            return Err(zbus::fdo::Error::InvalidArgs("Maximum out-of-range".into()));
+        };
+        debug!(
+            "Attaching to {sock} with memory in range [{minimum}, {maximum}]",
+            sock = socket.display()
+        );
+        self.machines
+            .write()
+            .await
+            .insert(vm::VM::new(socket, minimum, maximum), VMData::default());
+        Ok(())
+    }
+}
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to QMP socket
-    #[arg(short, long)]
-    socket: Vec<PathBuf>,
-
     /// Monitoring interval in seconds
-    #[arg(short, long, default_value_t = 1)]
-    interval: u64,
-
-    /// Minimum ballooning interval
     #[arg(short, long, default_value_t = 3)]
-    balloon_interval: u64,
-
-    /// Minimum memory size
-    #[arg(short, long, default_value_t = usize::MIN)]
-    minimum: usize,
-
-    /// Maximum memory size
-    #[arg(short = 'M', long, default_value_t = usize::MAX)]
-    maximum: usize,
+    interval: u64,
 
     /// Low memory presure
     #[arg(short, long, default_value_t = 70)]
     low: u8,
 
     /// High memory pressure
-    #[arg(short, long, default_value_t = 80)]
+    #[arg(short = 'H', long, default_value_t = 80)]
     high: u8,
-}
-
-#[derive(Debug)]
-struct MemoryStats {
-    balloon_size: usize,
-    base_memory: usize,
-    plugged_memory: usize,
-    total_memory: usize,
-    free_memory: usize,
-    available_memory: usize,
-}
-
-impl MemoryStats {
-    #[allow(clippy::cast_possible_truncation)]
-    pub fn pressure(&self) -> u8 {
-        ((201 * self.balloon_size - 200 * self.available_memory) / self.balloon_size / 2) as u8
-    }
-
-    pub fn reserved(&self) -> usize {
-        self.balloon_size - self.available_memory
-    }
-
-    pub fn adjusted(&self, target: u8) -> usize {
-        self.reserved() * 100 / target as usize
-    }
-
-    pub fn window(&self, min: u8, max: u8) -> Option<usize> {
-        let p = self.pressure();
-        if p < min {
-            Some(self.adjusted(min))
-        } else if p > max {
-            Some(self.adjusted(max - 2))
-        } else {
-            None
-        }
-    }
-}
-
-impl std::fmt::Display for MemoryStats {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "Memory stats:\n\
-             Balloon size: {} MiB\n\
-             Base memory: {} MiB\n\
-             Plugged memory: {} MiB\n\
-             Total memory: {} MiB\n\
-             Free memory: {} MiB\n\
-             Available memory: {} MiB",
-            self.balloon_size / 1024 / 1024,
-            self.base_memory / 1024 / 1024,
-            self.plugged_memory / 1024 / 1024,
-            self.total_memory / 1024 / 1024,
-            self.free_memory / 1024 / 1024,
-            self.available_memory / 1024 / 1024
-        )
-    }
-}
-
-async fn monitor_memory(args: Args) -> Result<()> {
-    let mut qmps: HashMap<_, (_, Option<Instant>)> = args
-        .socket
-        .iter()
-        .map(|p| (QmpEndpoint::new(p), (None, None)))
-        .collect();
-    let dur = Duration::from_secs(args.interval);
-    let bival = Duration::from_secs(args.balloon_interval);
-    let mut ival = tokio::time::interval(dur);
-    let mut errors = 0;
-    ival.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
-
-    loop {
-        ival.tick().await;
-        for (qmp, (last, last_balloon)) in &mut qmps {
-            let (conn, task, mut receiver) = match qmp.connect().await {
-                Ok(ctr) => ctr,
-                Err(e) => {
-                    warn!("Connection to {qmp} failed: {e}, trying again later",);
-                    continue;
-                }
-            };
-            if let Err(e) = tokio::select! {
-                e = async {
-                    conn.set_stats_interval(dur).await?;
-                    let balloon = conn.query_balloon().await?;
-                    let memory = conn.query_memory().await?;
-                    let guest_stats = conn.query_stats().await?;
-
-                    if last.replace(guest_stats.last_update) != Some(guest_stats.last_update) {
-                        let stats = MemoryStats {
-                            balloon_size: balloon.actual,
-                            base_memory: memory.base_memory,
-                            plugged_memory: memory.plugged_memory,
-                            total_memory: memory.base_memory + memory.plugged_memory,
-                            free_memory: guest_stats.stats.stat_free_memory,
-                            available_memory: guest_stats.stats.stat_available_memory,
-                        };
-
-                        debug!("Stats for {qmp}: {stats}, pressure: {}%", stats.pressure());
-                        if let Some(target) = stats
-                            .window(args.low, args.high)
-                            .map(|t| t.clamp(args.minimum, args.maximum))
-                            .filter(|&t| t != stats.balloon_size)
-                            .filter(|_| last_balloon.is_none_or(|l| l.elapsed() >= bival))
-                        {
-                            info!("Adjusting {qmp} balloon size from {} to {target}",
-                                stats.balloon_size);
-                            last_balloon.replace(Instant::now());
-                            conn.balloon(target).await?;
-                        }
-                    }
-                    Ok(())
-                } => e,
-                e = task => e,
-                () = {
-                    async move {
-                        while let Some(e) = receiver.recv().await {
-                            info!("Got event: {e:?}");
-                        }
-                    }
-                } => Ok(()),
-            } {
-                errors += 1;
-                if errors >= 5 {
-                    Err(e)?;
-                } else {
-                    warn!("Got error {e} with {qmp} for the {errors}th time");
-                }
-            } else {
-                errors = 0;
-            }
-        }
-    }
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let args = Args::parse();
-    monitor_memory(args).await
+    let manager = MemManager::new(args.low, args.high);
+    let _conn = zbus::connection::Builder::system()?
+        .name("ae.tii.MemManager")?
+        .serve_at("/", manager.clone())?
+        .build()
+        .await?;
+    let mut ival = tokio::time::interval(std::time::Duration::from_secs(args.interval));
+    let mut sigint = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())?;
+    ival.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
+
+    tokio::select! {
+        _ = sigint.recv() => Ok(()),
+        e = async move {
+            loop {
+                ival.tick().await;
+
+                if let Err(e) = manager.manage().await {
+                    warn!("Got error {e} managing VMs, trying again later");
+                }
+            }
+        } => e,
+    }
 }
