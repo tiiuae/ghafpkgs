63190bafcdb9cbe8cba6963c49be7263c08c3376
Brian McGillion <bmg.avoin@gmail.com>
2025-10-22 17:53:54 +0400

    overlays: remove the old remnants overlays file
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- packages/overlays.nix
+++
@@ -1,7 +0,0 @@
-# Copyright 2024 TII (SSRC) and the Ghaf contributors
-# SPDX-License-Identifier: Apache-2.0
-(_final: prev: {
-  ghaf-artwork = import ./ghaf-artwork { inherit prev; };
-  ghaf-audio-control = import ./ghaf-audio-control { inherit prev; };
-  ghaf-theme = import ./ghaf-theme { inherit prev; };
-})
