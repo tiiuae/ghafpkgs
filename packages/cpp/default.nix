3678233d31f44324a146e563f326fbd9115babf8
Brian McGillion <bmg.avoin@gmail.com>
2025-10-22 16:54:49 +0400

    cpp: refactor the cpp packages
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

---
+++ packages/cpp/default.nix
@@ -0,0 +1,9 @@
+# Copyright 2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{ pkgs }:
+let
+  inherit (pkgs) callPackage;
+in
+{
+  ghaf-audio-control = callPackage ./ghaf-audio-control { };
+}
