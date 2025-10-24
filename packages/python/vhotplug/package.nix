2c4771482dc5b2cf6e7c8339163b1db6519ec0a0
Ganga Ram <Ganga.Ram@tii.ae>
2025-10-24 13:37:02 +0400

    bump vhotplug
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- packages/python/vhotplug/package.nix
+++ packages/python/vhotplug/package.nix
@@ -20,8 +20,8 @@ buildPythonApplication {
   src = fetchFromGitHub {
     owner = "tiiuae";
     repo = "vhotplug";
-    rev = "8332c2e9e6ca19554eab90160ca161bf9a169a47";
-    hash = "sha256-+VRRPOXJuLrWfnf0uW7BZwhp/9LMsk6HIMpxqS3vqeA=";
+    rev = "70f5e0565f1bc71a30e0fef1f745f4fae82c2eda";
+    hash = "sha256-2yp4Rte9U8iZFrn6e5Lzrx/+GO98ZAOzwcR/xCv/7ws=";
   };
 
   build-system = [
