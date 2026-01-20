148d495d34acf4c88d5c0dd4f7477eb08007863e
Brian McGillion <bmg.avoin@gmail.com>
2026-01-20 17:32:34 +0400

    qubes-ctap: use python application
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- packages/python/qubes-ctap/package.nix
+++ packages/python/qubes-ctap/package.nix
@@ -3,15 +3,24 @@
 {
   yubico,
   fido2,
-  buildPythonPackage,
+  buildPythonApplication,
+  lib,
   packaging,
   setuptools,
   fetchFromGitHub,
 }:
-buildPythonPackage {
+
+buildPythonApplication {
   pname = "qubes-ctap";
   version = "2.0.6";
   pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "QubesOS";
+    repo = "qubes-app-u2f";
+    rev = "8a8866b48ba59ed81f52081e38a38484855f39a3";
+    sha256 = "sha256-9j/0yfaYQqBKROKDjphPOOvvWL1Qf1LuCW79ubT9mts=";
+  };
 
   dependencies = [
     yubico
@@ -19,24 +28,23 @@ buildPythonPackage {
     packaging
     setuptools
   ];
-  src = fetchFromGitHub {
-    owner = "QubesOS";
-    repo = "qubes-app-u2f";
-    rev = "8a8866b48ba59ed81f52081e38a38484855f39a3";
-    sha256 = "sha256-9j/0yfaYQqBKROKDjphPOOvvWL1Qf1LuCW79ubT9mts=";
-  };
+
   patches = [
     ./0001-fix-ghaf-Allow-changing-qrexec-path-from-cli.patch
   ];
+
+  postInstall = ''
+    mkdir -p $out/bin
+    cp $out/lib/python*/site-packages/usr/bin/* $out/bin/
+  '';
+
   meta = {
     description = "U2F token proxy app";
+    license = lib.licenses.gpl2Plus;
     platforms = [
       "aarch64-linux"
       "x86_64-linux"
     ];
+    mainProgram = "qctap-proxy";
   };
-  postInstall = ''
-    mkdir $out/bin
-    cp $out/lib/python*/site-packages/usr/bin/* $out/bin/
-  '';
 }
