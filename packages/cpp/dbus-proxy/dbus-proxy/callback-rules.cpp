bb09bad339ff58da8636469739fdefee81912cf7
jkuro-tii <jaroslaw.kurowski@tii.ae>
2026-04-14 14:36:24 +0400

    dbux-proxy: Add support for BlueZ
    Signed-off-by: jkuro-tii <jaroslaw.kurowski@tii.ae>

--- packages/cpp/dbus-proxy/dbus-proxy/callback-rules.cpp
+++ packages/cpp/dbus-proxy/dbus-proxy/callback-rules.cpp
@@ -5,8 +5,21 @@
 
 #include "dbus_proxy.h"
 
-const gchar* nm_agent_methods[] = {"GetSecrets", "CancelGetSecrets", "SaveSecrets", "DeleteSecrets",
-                                   nullptr};
+const GDBusMethodTable nm_agent_methods[] = {{"GetSecrets", "a{sa{sv}}osasu", "a{sa{sv}}"},
+                                             {"CancelGetSecrets", "os", ""},
+                                             {"SaveSecrets", "a{sa{sv}}o", ""},
+                                             {"DeleteSecrets", "a{sa{sv}}o", ""},
+                                             {nullptr, nullptr, nullptr}};
+
+const GDBusMethodTable bluez_agent_methods[] = {
+    {"RequestPinCode", "o", "s"},      {"DisplayPinCode", "os", ""},
+    {"RequestPasskey", "o", "u"},      {"DisplayPasskey", "ouq", ""},
+    {"RequestConfirmation", "ou", ""}, {"RequestAuthorization", "o", ""},
+    {"AuthorizeService", "os", ""},    {"Cancel", "", ""},
+    {nullptr, nullptr, nullptr}};
+
+const GDBusMethodTable obex_agent_methods[] = {
+    {"CreateSession", "s", "sa{sv}"}, {"RemoveSession", "o", ""}, {nullptr, nullptr, nullptr}};
 
 const AgentRule callbacks_rules[] = {
     {.bus_name = DBUS_NETWORK_MANAGER_NAME,
@@ -14,6 +27,7 @@ const AgentRule callbacks_rules[] = {
      .manager_interface = "org.freedesktop.NetworkManager.AgentManager",
      .register_method = "Register",
      .unregister_method = "Unregister",
+     .use_object_path_on_unregister = FALSE,
      .object_path_customisable = FALSE,
      .client_object_path = DBUS_NM_AGENT_PATH,
      .client_interface = DBUS_INTERFACE_SECRET_AGENT,
@@ -24,12 +38,38 @@ const AgentRule callbacks_rules[] = {
      .manager_interface = "org.freedesktop.NetworkManager.AgentManager",
      .register_method = "RegisterWithCapabilities",
      .unregister_method = "Unregister",
+     .use_object_path_on_unregister = FALSE,
      .object_path_customisable = FALSE,
 
      .client_object_path = DBUS_NM_AGENT_PATH,
      .client_interface = DBUS_INTERFACE_SECRET_AGENT,
      .client_methods = nm_agent_methods},
-    {nullptr, nullptr, nullptr, nullptr, nullptr, FALSE, nullptr, nullptr, nullptr}};
+
+    {.bus_name = DBUS_BLUEZ_NAME,
+     .manager_path = "/org/bluez",
+     .manager_interface = "org.bluez.AgentManager1",
+     .register_method = "RegisterAgent",
+     .unregister_method = "UnregisterAgent",
+     .use_object_path_on_unregister = TRUE,
+     .object_path_customisable = FALSE,
+
+     .client_object_path = DBUS_BLUEZ_AGENT_PATH,
+     .client_interface = DBUS_BLUEZ_AGENT_INTERFACE,
+     .client_methods = bluez_agent_methods},
+
+    {.bus_name = DBUS_OBEX_NAME,
+     .manager_path = "/org/bluez",
+     .manager_interface = "org.bluez.obex.AgentManager1",
+     .register_method = "RegisterAgent",
+     .unregister_method = "UnregisterAgent",
+     .use_object_path_on_unregister = FALSE,
+     .object_path_customisable = FALSE,
+
+     .client_object_path = DBUS_OBEX_AGENT_PATH,
+     .client_interface = DBUS_OBEX_AGENT_INTERFACE,
+     .client_methods = obex_agent_methods},
+
+    {nullptr, nullptr, nullptr, nullptr, nullptr, FALSE, FALSE, nullptr, nullptr, nullptr}};
 
 const AgentRule* get_callback_rule(const gchar* bus_name, const gchar* interface_name,
                                    const gchar* method_name) {
